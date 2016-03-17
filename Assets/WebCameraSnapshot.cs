fe92f97c7a7275bcc76fcd432bebd89f626b4e12
unknown <piotrek.wawryka@gmail.com>
2016-03-17 12:23:27 +0100

    get input from webcamera and put it to the canvas

---
+++ Assets/WebCameraSnapshot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class WebCameraSnapshot : MonoBehaviour {
+    WebCamTexture webCamTexture;
+
+    // Use this for initialization
+    void Start() {
+        webCamTexture = new WebCamTexture();
+        GetComponent<RawImage>().texture = webCamTexture;
+        GetComponent<RawImage>().material.mainTexture = webCamTexture;
+        webCamTexture.Play();
+    }
+
+}
