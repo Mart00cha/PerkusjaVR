6b6612d001f57a55e1484177f56f40321106b03e
unknown <piotrek.wawryka@gmail.com>
2016-04-25 20:36:26 +0200

    Added ScoreManager with canvas on the screen

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public enum POINTS_TYPES {
+        GOOD = 20,
+        BAD = -10
+    };
+
+    Text scoreText;
+    private long score = 0;
+
+    void Start () {
+        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+    }
+	
+	void Update () {
+        scoreText.text = "SCORE: " + score;
+	}
+
+    public void ApplyPoints(POINTS_TYPES points) {
+        score += (long)points;
+    }
+}
