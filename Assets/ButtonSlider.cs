aac02b6792f02b97c9ef05ba36c8ab5c6d528117
Mikolaj Nowak <mikolaj.nowak93@gmail.com>
2016-06-02 18:05:01 +0200

    Falling notes added

---
+++ Assets/ButtonSlider.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonSlider : MonoBehaviour {
+	int speed = 1;
+	float alpha = 1f;
+	float fadeOutSpeed = 2f;
+
+	void Update () {
+		Vector3 poz = transform.position;
+
+		if (poz.z > 0.5) {
+			transform.Translate (new Vector3 (0, 0, -1) * Time.deltaTime * speed);
+		}
+		else {
+			HideObject ();
+
+		}
+	}
+
+	void HideObject() {
+		alpha -= (fadeOutSpeed*Time.deltaTime);
+		if (alpha < 0) {
+			this.gameObject.SetActive (false);			
+		} else {
+			Color temp = this.gameObject.GetComponent<Renderer> ().material.color;
+			temp.a = alpha;
+			this.gameObject.GetComponent<Renderer> ().material.color = temp;
+		}
+	}
+}
