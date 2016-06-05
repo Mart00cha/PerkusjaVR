f547a9bd15440a2c9ec2756791c61926967c7e8c
Mikolaj Nowak <mikolaj.nowak93@gmail.com>
2016-06-05 23:11:07 +0200

    WIP commit

--- Assets/ButtonSlider.cs
+++ Assets/ButtonSlider.cs
@@ -1,26 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class ButtonSlider : MonoBehaviour {
 	int speed = 1;
 	float alpha = 1f;
 	float fadeOutSpeed = 2f;
+	float epsilon = 0.5f;
+	float optimumHitZ = 0f;
 
 	void Update () {
-		Vector3 poz = transform.position;
+		if (this.gameObject.activeSelf) {
+			Vector3 poz = transform.position;
 
-		if (poz.z > 0.5) {
 			transform.Translate (new Vector3 (0, 0, -1) * Time.deltaTime * speed);
-		}
-		else {
-			HideObject ();
-
+			if (poz.z < 0.5f) {
+				HideObject ();
+			}
 		}
 	}
 
 	void HideObject() {
 		alpha -= (fadeOutSpeed*Time.deltaTime);
-		if (alpha < 0) {
+		if (alpha < 0f) {
 			this.gameObject.SetActive (false);
 			ScoreManager manager = GameObject.Find("Score").GetComponent<ScoreManager> ();
 			manager.ApplyPoints (ScoreManager.POINTS_TYPES.GOOD);
@@ -29,5 +31,12 @@ public class ButtonSlider : MonoBehaviour {
 			temp.a = alpha;
 			this.gameObject.GetComponent<Renderer> ().material.color = temp;
 		}
+	}
+
+	public bool HitCorrect() {
+		if (Math.Abs (transform.position.z - optimumHitZ) < epsilon) {
+			return true;
+		}
+		return false;
 	}
 }
