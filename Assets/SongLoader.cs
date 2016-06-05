ea00e03b0561998e33fe6399c214e47d45a3aaa4
Marta Panuszewska <mpanuszewska@gmail.com>
2016-06-06 01:35:46 +0200

    add audience

--- Assets/SongLoader.cs
+++ Assets/SongLoader.cs
@@ -43,12 +43,12 @@ public class SongLoader : MonoBehaviour {
 
 	void AddButton(int buttonId, int secondOfPlaying) {
 		GameObject button = (GameObject) Instantiate(Resources.Load("Prefabs/MiddleButton"), new Vector3(0,0,0), Quaternion.identity);
-		GameObject plane = GameObject.Find("TrackButton").GetComponent<TrackButton> ();
-		button.transform.parent = 
-		button.SetActive (false);
-		if(!buttons.ContainsKey(secondOfPlaying)) {
-			buttons [secondOfPlaying] = new ArrayList ();
-		}
-		buttons[secondOfPlaying].Add(button);
+//		GameObject plane = GameObject.Find("TrackButton").GetComponent<TrackButton> ();
+//		button.transform.parent = 
+//		button.SetActive (false);
+//		if(!buttons.ContainsKey(secondOfPlaying)) {
+//			buttons [secondOfPlaying] = new ArrayList ();
+//		}
+//		buttons[secondOfPlaying].Add(button);
 	}
 }
